593ea421356478aaf71c4047531e8368a19b3e57
selodev <selahattin47usa@hotmail.com>
2021-02-01 09:11:52 +0000

    added mkcert

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,14 +5,14 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(docker.selo.dev)
+domains=(selo.dev)
 rsa_key_size=4096
 data_path="./webserver2/certbot"
 email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  read -p "Cfor $domains. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
