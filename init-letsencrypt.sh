7f3eeaba8081a0848e44b5375cf3224c1882de7e
selodev <selahattin47usa@hotmail.com>
2021-02-02 04:22:00 +0000

    updated docker files

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -12,7 +12,7 @@ email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  read -p "Cfor $domains. Continue and replace existing certificate? (y/N) " decision
+  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
